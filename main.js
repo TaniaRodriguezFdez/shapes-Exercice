b7b7cb3e11a3023e701bcbb84fd3bb2f0687c199
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-05-14 16:21:07 +0200

    refresh

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 
-let btn=document.querySelector('button');
-let div=document.querySelector('div');
+let btn = document.querySelectorAll('button');
+let reset = document.getElementById('reset')
+// let div = document.querySelector('div');
 
-div.addEventListener('click', () => {
-    if(btn.style.display === 'none'){
-         btn.style.display='block';
-    }
-    else{
-         btn.style.display='none';
-     }          I
+// div.addEventListener('click', () => {
+//     if(btn.style.display === 'none'){
+//          btn.style.display='';
+//     }
+//     else{
+//          btn.style.display='none';
+//      }          I
+// });
+
+
+
+let refresh = document.getElementById('reset');
+refresh.addEventListener('click', _ => {
+            location.reload();
 })
 
-document.getElementById("reset").onclick = function() {
-    btn.style.display ="";
-};
+btn.forEach(function (item){
+    console.log(item);
+    item.addEventListener('click', function() {
+        item.style.display = "none";
+    })
+
+})
